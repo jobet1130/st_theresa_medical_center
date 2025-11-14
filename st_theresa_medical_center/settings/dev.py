a242906fbc3957e0288a3421f26a9ffad4219cfe
jobet1995 <jobetcasquejo221@gmail.com>
2025-11-14 11:40:29 +0800

    Initialize

---
+++ st_theresa_medical_center/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7&uf=vye&sff_15roh@ne6wknp_^#+e0!s@lmp3ey23r!!pinb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
